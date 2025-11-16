5d50efbefae4545bdd4110e24e0791b8dbff5c32
fish19-12 <155108286+fish19-12@users.noreply.github.com>
2025-11-16 17:37:17 +0300

    Update api.js
    edit backend url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL ="https://auth-system-1-ik42.onrender.com";
 
 // Modern clean POST function
 export const apiPost = async (path, body = {}) => {
